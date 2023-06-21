c260edc1dabba626cd309256948714361beab454
TobiLight <jgetitdonefast@gmail.com>
2023-06-22 00:29:55 +0100

    ...

--- tokenizer.c
+++ tokenizer.c
@@ -47,7 +47,7 @@ void tokenizer(void)
 		n++;
 	}
 	printf("Token sizee: %d\n", n);
-	printf("Tok: %s\n", *(arguments)->tokens);
+	printf("Tok: %s\n", arguments->tokens[0]);
 
 	arguments->tokens[n] = NULL;
 	free(line_cpy);
