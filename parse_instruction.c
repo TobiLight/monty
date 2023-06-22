75e91ba9f060e1c824beb240a102f20375971336
TobiLight <jgetitdonefast@gmail.com>
2023-06-22 23:00:51 +0100

    Modified code

--- parse_instruction.c
+++ parse_instruction.c
@@ -28,11 +28,11 @@ void parse_instruction(void)
 		arguments->instruction->f = nop;
 		return;
 	}
-	for (i = 0; instruction[i].opcode != NULL; i++)
+	for (i = 0; instruction[i]->opcode != NULL; i++)
 	{
-		if (strcmp(instruction[i].opcode, arguments->tokens[0]) == 0)
+		if (strcmp(instruction[i]->opcode, arguments->tokens[0]) == 0)
 		{
-			arguments->instruction->opcode = instruction[i].opcode;
+			arguments->instruction->opcode = instruction[i]->opcode;
 			arguments->instruction->f = instruction[i].f;
 			return;
 		}
