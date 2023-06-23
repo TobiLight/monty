a623b39fca63c1c73038b96a906ccd00b6c80571
TobiLight <jgetitdonefast@gmail.com>
2023-06-23 02:12:38 +0100

    modified code

--- operations.c
+++ operations.c
@@ -61,12 +61,8 @@ void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (arguments->head == NULL)
-		return;
-
 	(void)line_number;
-	(void)stack;
-	temp = arguments->head;
+	temp = *stack;
 	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
