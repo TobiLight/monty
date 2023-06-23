12a0efbf4ef09aa5ae6e0d134071c3db7fdf2ff6
TobiLight <jgetitdonefast@gmail.com>
2023-06-23 02:41:12 +0100

    modified code

--- operations.c
+++ operations.c
@@ -86,3 +86,23 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)stack;
 	(void)line_number;
 }
+
+
+/**
+ * pint - Prints the value at the top of the stack.
+ * @stack: Pointer to the stack.
+ * @line_number: Line number where the pint function is called.
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+
+	if (arguments->head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		free_all_args();
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", arguments->head->n);
+}
