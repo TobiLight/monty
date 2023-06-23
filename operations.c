8ff36b40f0154d433d84cc18eb44b5e0483dd7d2
TobiLight <jgetitdonefast@gmail.com>
2023-06-23 02:06:48 +0100

    modified code

--- operations.c
+++ operations.c
@@ -60,7 +60,8 @@ void push(stack_t **stack, unsigned int line_number)
 void pall(stack_t **stack, unsigned int line_number)
 {
 	stack_t *head;
-	(void)line;
+	(void)line_number;
+	
 	head = *stack;
 	while (head != NULL)
 	{
